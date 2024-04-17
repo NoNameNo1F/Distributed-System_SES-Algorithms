cd42bcc851e2c14dbecb77c9179503694adf67e3
Giahyyyy <hy3132002@gmail.com>
2024-04-17 09:31:54 +0700

    feat(core): fixing block thread

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import json
 
 from dotenv import load_dotenv
 
@@ -13,43 +14,59 @@ def client():
     client_socket = socket.socket()
     client_socket.connect((host, port))
     logger = Logging("..\logs","logs.txt")
-
+    
+    connected_clients = []
+    
     client_socket.sendall(str((host, port)).encode())
 
     connected_clients = client_socket.recv(1024).decode()
     connected_clients = eval(connected_clients)
     logger.Log(f"Connected clients: {connected_clients}", "INFO")
+    #print("hy")
+    
+    def receive_client_list():
+        connected_clients = client_socket.recv(1024).decode()
+        print("Chuỗi nhận từ sv:", connected_clients)
+        return json.loads(connected_clients)
 
-    other_clients = []
+    
+    def update_client_list():
+        connected_clients = receive_client_list()
+        logger.Log(f"Connected clients: {connected_clients}", "INFO")
+        
+        
+    while True:
+        update_client_list()
+    # other_clients = []
 
-    """
-        Nếu có site khác thì sẽ thêm vào other_clients
-    """
-    if (host, port) in connected_clients:
-        connected_clients.remove((host, port))
-        other_clients = connected_clients
+    # """
+    #     Nếu có site khác thì sẽ thêm vào other_clients
+    # """
+    # if (host, port) in connected_clients:
+    #     connected_clients.remove((host, port))
+    #     other_clients = connected_clients
 
-        print("Other clients:", other_clients)
+    #     print("Other clients:", other_clients)
 
-    message = input(' -> ')
-    while message.lower().strip() != 'bye':
-        if len(other_clients) > 0:
-            # Chọn site muốn communicate
-            print("Select a client to communicate with:")
-            for index, client_addr in enumerate(other_clients, start=1):
-                print(f"{index}. {client_addr}")
-            client_idx = int(input("Enter the client number: ")) - 1
+    # message = input(' -> ')
+    # while message.lower().strip() != 'bye':
+    #     if len(other_clients) > 0:
+    #         # Chọn site muốn communicate
+    #         print("Select a client to communicate with:")
+    #         for index, client_addr in enumerate(other_clients, start=1):
+    #             print(f"{index}. {client_addr}")
+    #         client_idx = int(input("Enter the client number: ")) - 1
 
-            # Send message to selected client
-            selected_client = other_clients[client_idx]
-            client_socket.sendall(selected_client.encode())
-        else:
-            print("No other clients to communicate with.")
+    #         # Send message to selected client
+    #         selected_client = other_clients[client_idx]
+    #         client_socket.sendall(selected_client.encode())
+    #     else:
+    #         print("No other clients to communicate with.")
 
-        data = client_socket.recv(1024).decode()
+    #     data = client_socket.recv(1024).decode()
 
-        logger.Log(f"Received from server: {data}", "INFO")
+    #     logger.Log(f"Received from server: {data}", "INFO")
 
-        message = input(' -> ')
+    #     message = input(' -> ')
     client_socket.close()
 
