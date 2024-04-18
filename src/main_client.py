8b7bea23abc3adc192575dec9211fa711037fa34
20127670 <nonameno1f@gmail.com>
2024-04-19 01:42:54 +0700

    feat: updating main client

--- src/main_client.py
+++ src/main_client.py
@@ -2,8 +2,21 @@ import os
 
 from dotenv import load_dotenv
 
-from Client.client import client
+from Client.client import ClientYLS
+from core.Message.message_type import MessageType
 
 if __name__ == "__main__":
-    client()
-
+    # #client()
+    # # for a in MessageType:
+    # #     print(str(a.name).lower())
+    # command = 'read'
+    # if command in MessageType.to_dict():
+    #     print("trueaaa")
+    # if command in MessageType.to_list():
+    #     print("trueaaaaa")
+    # for i in MessageType.to_list():
+    #     print(i)
+    # # if command in str(MessageType.name).lower():
+    # #     print("true")
+    client = ClientYLS()
+    client.client_run()
