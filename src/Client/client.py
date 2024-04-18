c4c61d94b457d43728517f088109d0226e983930
20127670 <nonameno1f@gmail.com>
2024-04-19 01:42:15 +0700

    feat(core): updating sendbroadcastsite

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,72 +1,100 @@
+import json
 import os
 import socket
-import json
 
 from dotenv import load_dotenv
 
+from core.Message.message_type import MessageType
 from utils.loggings import Logging
 
 
-def client():
+class ClientYLS:
+    # load HOST, PORT from .ENV
     config = load_dotenv()
-    host = os.environ.get('HOST')
-    port = int(os.environ.get('PORT'))
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    logger = Logging("..\logs","logs.txt")
-    
-    connected_clients = []
-    
-    client_socket.sendall(str((host, port)).encode())
+    HOST = os.environ.get('HOST')
+    PORT = int(os.environ.get('PORT'))
 
-    connected_clients = client_socket.recv(1024).decode()
-    connected_clients = eval(connected_clients)
-    logger.Log(f"Connected clients: {connected_clients}", "INFO")
-    #print("hy")
-    
-    def receive_client_list():
-        connected_clients = client_socket.recv(1024).decode()
-        print("Chuỗi nhận từ sv:", connected_clients)
-        return json.loads(connected_clients)
+    def __init__(self) -> None:
+        self._sites = {} # ADDRESS of other sites
+        self._logger = Logging("../logs","logs.txt")
+        self._clientSocket = socket.socket()
+        self._clientSocket.connect((ClientYLS.HOST, ClientYLS.PORT))
+        self._address = self._clientSocket.getsockname()
 
-    
-    def update_client_list():
-        connected_clients = receive_client_list()
-        logger.Log(f"Connected clients: {connected_clients}", "INFO")
-        
-        
+    def updating_sites_address(self, addresses):
+        for addr in addresses:
+            if addr not in self._sites.keys():
+                self._sites[addr] = len(self._sites) + 1
+    def client_run(self):
+        while True:
+            received_data, sender = self._clientSocket.recv(1024).decode()
+
+            try:
+                data = json.loads(received_data)
+                print(data)
+            except json.JSONDecodeError as e:
+                self._logger.Log(f"Invalid JSON format received: {received_data}", "ERROR")
+                continue
+            print(sender)
+            self._logger.Log(f"RECEIVED FROM {sender} - {data}", "INFO")
+
+            #received_data = eval(received_data)
+            print(f"{type(received_data)} : {received_data}")
+            print(f"Thông tin site từ server: {received_data}")
+
+            self.updating_sites_address(received_data)
+
+            print("Other clients:", self._sites)
+
+            # data = client_socket.recv(1024).decode()
+
+            # self._logger.Log(f"Received from server: {data}", "INFO")
+        client_socket.close()
+
+    def handle_command(self, command: str, args):
+        self._logger.Log(f"{self._address}: {command} {args}", "INFO")
+        match command:
+            case 'broadcast':
+                dosomeigh= []
+            case 'get':
+                return
+            case 'read':
+                return
+            case 'write':
+                return
+            case 'start_writing':
+                return
+            case 'stop_writing':
+                return
+    def display_user_options(self):
+        if len(self._sites) > 0:
+            # Chọn site muốn communicate
+            print("Select a client to communicate with:")
+            for addr, index in self._sites.items():
+                print(f"{index}. {addr}")
+
+            # selected_client = other_clients[client_idx]
+            # client_socket.sendall(f"selected_client Hahaha{selected_client}".encode())
+
+def handle_site_command(client: ClientYLS):
+    """
+        tham số 1 - [0]: các lệnh READ/WRITE/GET/BROADCAST/
+        tham số 2 - [1]: giá trị args cho command
+    """
+
     while True:
-        update_client_list()
-    # other_clients = []
+        client.display_user_options()
+        user_command = input(f"{client._address}$ ").split()
 
-    # """
-    #     Nếu có site khác thì sẽ thêm vào other_clients
-    # """
-    # if (host, port) in connected_clients:
-    #     connected_clients.remove((host, port))
-    #     other_clients = connected_clients
+        command = user_command[0].lower()
+        arg = user_command[1].lower()
 
-    #     print("Other clients:", other_clients)
+        if command in MessageType.to_dict():
+            client.handle_command(command, arg)
 
-    # message = input(' -> ')
-    # while message.lower().strip() != 'bye':
-    #     if len(other_clients) > 0:
-    #         # Chọn site muốn communicate
-    #         print("Select a client to communicate with:")
-    #         for index, client_addr in enumerate(other_clients, start=1):
-    #             print(f"{index}. {client_addr}")
-    #         client_idx = int(input("Enter the client number: ")) - 1
+        if command == 'exit':
+            client._logger.Log(f"{client._address} disconnected server", "INFO")
+            return
 
-    #         # Send message to selected client
-    #         selected_client = other_clients[client_idx]
-    #         client_socket.sendall(selected_client.encode())
-    #     else:
-    #         print("No other clients to communicate with.")
-
-    #     data = client_socket.recv(1024).decode()
-
-    #     logger.Log(f"Received from server: {data}", "INFO")
-
-    #     message = input(' -> ')
-    client_socket.close()
-
+        else:
+            client._logger.Log(f"{client._address} command not appropriate {command} {arg}", "INFO")
--- src/Server/server.py
+++ src/Server/server.py
@@ -10,60 +10,129 @@ from utils.loggings import Logging
 
 
 class Server:
+    """
+        client_socket: type socket
+        address: type tuple
+
+        Server khi nhận yêu cầu connect từ client , sẽ gửi thông tin các client
+        còn lại cho connect mới, và cũng như update cho các sites còn lại
+    """
+    config = load_dotenv()
+    HOST = os.environ.get('HOST')
+    PORT = int(os.environ.get('PORT'))
     def __init__(self):
         self._clients = {}
         self._lock = threading.Lock()
-        self._logger = Logging("..\logs", "server_logs.txt")
-        
-    def BroadcastClientList(self):
-        with self._lock:
-            client_list = list(self._clients.keys())
-            client_data = json.dumps(client_list).encode()
-            print("data123",client_data)
-            for client_socket in self._clients.values():
-                client_socket.sendall(client_data)
+        self._logger = Logging("../logs", "server_logs.txt")
 
-    def HandleClientConnected(self, client_socket, address):
-        self._logger.Log(f"Connection from {address}", "INFO")
+        self._serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._serverSocket.bind((Server.HOST,Server.PORT))
+
+    def send_broadcast_sites_update(self, client_socket):
+        with self._lock:
+            #sites = self._clients.copy()
+            for addr, sock in self._clients.items():
+                sites = self._clients.copy()
+                print(sites)
+                del sites[addr]
+                if len(sites) > 0:
+                    #self._serverSocket.sendall(json.dumps(sites).encode(), addr)
+                    client_socket.sendall(json.dumps(sites).encode())
+                else:
+                    #self._serverSocket.sendall(json.dumps('You are connected').encode())
+                    client_socket.sendall(json.dumps(sites).encode())
+                # if addr in sites.keys():
+                #     del sites[addr]
+                #     self._serverSocket.sendto(json.dumps(sites).encode(), addr)
+                # client_socket.sendall(json.dumps(update_item).encode())
+                # if item != address:
+                #     print(f"{item} : {type(item)} {self._clients[item]} : {type(self._clients[item])}")
+                #     update_item.append(item)
+
+    def handle_client_connected(self, client_socket, address):
+        self._logger.Log(f"Adding Connection {address} to pool", "INFO")
+        # Them sites vao trong server
         with self._lock:
             self._clients[address] = client_socket
 
-            #client_socket.sendall(str(list(self._clients.keys())).encode())
-            ##client_socket.sendall(json.dumps(list(self._clients.keys())).encode())
-            
-           
+        self.send_broadcast_sites_update(client_socket)
+
+        # Nhận data từ các sites
         while True:
-            data = client_socket.recv(1024).decode()
-            if not data:
+            data = client_socket.recv(2048).decode()
+            #data = self._serverSocket.recv(1024).decode()
+            if data == 'exit' or not data:
+                self._logger.Log(f"Sites: {data.getsockname()} sent Disconnect", "INFO")
                 break
 
             self._logger.Log(f"from connected Site: {data}", "INFO")
-            self.BroadcastClientList()
-            
             # with self._lock:
             #     for addr, sock in self._clients.items():
             #         if sock != client_socket:
             #             sock.sendall(data.encode())
 
-        # with self._lock:
-        #     del self._clients[address]
+        with self._lock:
+            del self._clients[address]
         client_socket.close()
 
-    def ServerRun(self):
-        config = load_dotenv()
-        host = os.environ.get('HOST')
-        port = int(os.environ.get('PORT'))
+    def server_run(self):
+        self._serverSocket.listen(2)
 
-        server_socket = socket.socket()
-
-        server_socket.bind((host, port))
-
-        server_socket.listen(2)
-
-        self._logger.Log(f"Server Start at {host}:{port}","INFO")
+        self._logger.Log(f"Server Start at {self.HOST}:{self.PORT}","INFO")
 
         while True:
-            client_socket, address = server_socket.accept()
-            threading.Thread(target=self.HandleClientConnected, \
-                            args= (client_socket, address)).start()
+            client_socket, address = self._serverSocket.accept()
+            server_receive = threading.Thread(target=self.handle_client_connected, \
+                            args= (client_socket, address))
+            server_receive.setDaemon(True)
+            server_receive.start()
+# import json
+# import os
+# import socket
 
+# from dotenv import load_dotenv
+
+# from utils.loggings import Logging
+
+
+# class Server:
+#     def __init__(self):
+#         self._clients = {}
+#         self._lock = None  # No need for threading lock
+#         self._logger = Logging("../logs", "server_logs.txt")
+
+#     def HandleClientConnected(self, client_socket, address):
+#         self._logger.Log(f"Connection from {address}", "INFO")
+#         self._clients[address] = client_socket
+
+#         client_socket.sendall(json.dumps(list(self._clients.keys())).encode())
+
+#         while True:
+#             data = client_socket.recv(1024).decode()
+#             if not data:
+#                 break
+
+#             self._logger.Log(f"from connected Site: {data}", "INFO")
+#             for addr, sock in self._clients.items():
+#                 if sock != client_socket:
+#                     sock.sendall(data.encode())
+
+#         del self._clients[address]
+#         client_socket.close()
+
+#     def ServerRun(self):
+#         config = load_dotenv()
+#         host = os.environ.get('HOST')
+#         port = int(os.environ.get('PORT'))
+
+#         server_socket = socket.socket()
+#         server_socket.bind((host, port))
+#         server_socket.listen(2)
+
+#         self._logger.Log(f"Server Start at {host}:{port}","INFO")
+
+#         while True:
+#             client_socket, address = server_socket.accept()
+#             self.HandleClientConnected(client_socket, address)
+
+#         server_socket.close()  # Close the server socket when done
