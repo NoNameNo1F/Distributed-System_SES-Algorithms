2fb9ba8fc391cab435cf9e9152dfaea48634934a
20127670 <nonameno1f@gmail.com>
2024-04-20 10:02:09 +0700

    feat(core): adding message type

--- src/core/Message/message_type.py
+++ src/core/Message/message_type.py
@@ -4,12 +4,11 @@ from enum import Enum
     MessageType: Type of messages
 """
 class MessageType(Enum):
-    BROADCAST = 1
-    GET = 2
-    READ = 3
-    WRITE = 4
-    START_WRITING = 5
-    STOP_WRITING = 6
+    MOUNT = 1
+    READ = 2
+    WRITE = 3
+    START_WRITING = 4
+    STOP_WRITING = 5
 
     def to_dict() -> dict:
         dict = {}
