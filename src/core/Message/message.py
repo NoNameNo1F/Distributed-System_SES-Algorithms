333446d1fdfdc2c82458468f6355151ba1592fe1
20127670 <nonameno1f@gmail.com>
2024-04-20 09:59:58 +0700

    feat(core): add message object to sent peer to peer

--- src/core/Message/message.py
+++ src/core/Message/message.py
@@ -1,12 +1,36 @@
+import json
 
 from core.Message.message_type import MessageType
 
 
 class Message:
-    def __init__(self, sender: int, receiver: int, messageType: MessageType, timestamp: list, vp: dict, messageContent: str) -> None:
+    def __init__(self, sender: str, receiver: str, messageType: int, messageContent: str, timestamp: dict, vp: dict) -> None:
         self._sender = sender
         self._receiver = receiver
         self._messageType = messageType
+        self._messageContent = messageContent
         self._timestamp = timestamp
         self._vp = vp
-        self._messageContent = messageContent
+    """
+        return from dict to Message
+    """
+    def from_dict(self, message):
+        return Message(
+            message["sender"],
+            message["receiver"],
+            message["message"],
+            message["timestamps"],
+            message["vp"],
+            message["message_type"],
+        )
+
+    """
+        load dict from string
+    """
+    def from_string(self, message):
+        return Message.from_dict(json.loads(message))
+    """
+        convert dict to string
+    """
+    def to_string(self) -> str:
+        return json.dumps(self.__dict__)
