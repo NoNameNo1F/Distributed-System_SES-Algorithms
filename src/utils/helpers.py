ebda386329a35e8c28c52471b4fc5cbc5dd2f4d5
20127670 <nonameno1f@gmail.com>
2024-04-20 10:06:45 +0700

    feat(core): adj the correct normpath, add function to get , and setup folder of an sites

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -6,16 +6,39 @@
 import os
 
 
+def get_foldershares_path():
+    """
+        Function: get foldershares path
+    """
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(current_dir, '../../FolderShares')
+    return os.path.normpath(path)
+
 def get_data_file_path(dir, filename):
+    """
+        Get specific file path
+    """
     # Get the path to the data directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(current_dir, dir)
     # Return the path to the data.json file
-    return os.path.join(data_dir, filename)
+    path = os.path.join(data_dir, filename)
+    return os.path.normpath(path)
 
-def create_folder(port):
-    site_folder = f"Site{port}"
-    os.makedirs(site_folder, exist_ok=True)
+def create_folders(site_folder, site_subfolders, other_sites_subfolders):
+    """
+        setup folder to mounting when initializing connection
+    """
+    # Create the main folder
+    parent_folder = get_foldershares_path()
+    main_path = os.path.join(parent_folder, site_folder)
+    os.makedirs(main_path, exist_ok=True)
 
-    peer_folder = os.path.join(site_folder, f"Peer{port}")
-    os.makedirs(peer_folder, exist_ok=True)
+    # Create subfolders inside the main folder
+    for folder in site_subfolders:
+        os.makedirs(os.path.join(main_path, folder), exist_ok=True)
+
+        # If the subfolder is 'OtherSites', create additional subfolders
+        if folder == 'OtherSites':
+            for subfolder in other_sites_subfolders:
+                os.makedirs(os.path.join(main_path, folder, subfolder), exist_ok=True)
