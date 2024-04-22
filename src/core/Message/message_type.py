6b37056fcd968964bab6e2189032e9e6cda2eac8
20127670 <nonameno1f@gmail.com>
2024-04-22 20:37:11 +0700

    feat(core): removing broadcast type

--- src/core/Message/message_type.py
+++ src/core/Message/message_type.py
@@ -4,11 +4,19 @@ from enum import Enum
     MessageType: Type of messages
 """
 class MessageType(Enum):
-    MOUNT = 1
-    READ = 2
-    WRITE = 3
-    START_WRITING = 4
-    STOP_WRITING = 5
+    # Type when sending a message
+    SEND_MOUNT = 1
+    SEND_READ = 2
+    SEND_WRITE = 3
+    SEND_START_WRITING = 4
+    SEND_STOP_WRITING = 5
+
+    # Type When receiving a message and then reply back
+    RECEIVE_MOUNT = -1
+    RECEIVE_READ = -2
+    RECEIVE_WRITE = -3
+    RECEIVE_START_WRITING = -4
+    RECEIVE_STOP_WRITING = -5
 
     def to_dict() -> dict:
         dict = {}
