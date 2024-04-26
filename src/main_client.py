ac3ff4c3699b84128056611542954e17ff4c29da
20127670 <nonameno1f@gmail.com>
2024-04-27 00:09:18 +0700

    chore: updating

--- src/main_client.py
+++ src/main_client.py
@@ -12,17 +12,16 @@ if __name__ == "__main__":
     if(len(sys.argv) == 2):
         input_port = int(sys.argv[1])
         if input_port in range(7670,7675):
-            time.sleep(2)
+            #time.sleep(2)
             client = PeerYFS(input_port)
-            client_yls = threading.Thread(target=client.client_listen_event, args=())
-            client_input = threading.Thread(target=client.handle_site_command, args=())
-            #client_input = threading.Thread(target=handle_site_command, args=(client,))
-            #client_yls.daemon = True
+            client_yls = threading.Thread(target=client.client_listen_event)
+            client_input = threading.Thread(target=client.handle_site_command)
+
             client_yls.start()
             client_input.start()
 
-            # client_yls.join()
-            # client_input.join()
+            client_yls.join()
+            client_input.join()
 
     else:
         print(f'command require PORT or PORT not appropriate to initialize the client')
